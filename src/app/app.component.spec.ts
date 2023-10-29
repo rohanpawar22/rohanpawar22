c4903a7808a96f5cae15914367f39647d13b955b
Rohan <rohan@onnetsystems.net>
2023-10-30 00:15:03 +0530

    New build

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-CV'`, () => {
+  it(`should have as title 'rohanpawar22'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-CV');
+    expect(app.title).toEqual('rohanpawar22');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-CV app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('rohanpawar22 app is running!');
   });
 });
